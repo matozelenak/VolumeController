c133fe6e6c31b659bbfdb490af8a3763e0fbef18
matozelenak <matoserver1@gmail.com>
2025-04-10 17:32:49 +0200

    add Channel

---
+++ linux/src/channel.cpp
@@ -0,0 +1,86 @@
+#include "channel.h"
+#include "volume_manager.h"
+
+#include <memory>
+using namespace std;
+
+Channel::Channel(shared_ptr<VolumeManager> mgr)
+    : _mgr(mgr) {
+    _active = false;
+    _volume = 0;
+    _mute = false;
+}
+
+Channel::~Channel() {
+
+}
+
+bool Channel::addDevice(Session &device) {
+    _devices[device.index] = device;
+    _mgr->setSinkVolume(device.index, _volume);
+    _mgr->setSinkMute(device.index, _mute);
+    if (!_active) {
+        _active = true;
+        return true;
+    }
+    return false;
+}
+
+bool Channel::addSession(Session &session) {
+    _sessions[session.index] = session;
+    _mgr->setSinkInputVolume(session.index, _volume);
+    _mgr->setSinkInputMute(session.index, _mute);
+    if (!_active) {
+        _active = true;
+        return true;
+    }
+    return false;
+}
+
+bool Channel::removeDevice(int index) {
+    _devices.erase(index);
+    if (_devices.size() + _sessions.size() == 0) {
+        _active = false;
+        return true;
+    }
+    return false;
+}
+
+bool Channel::removeSession(int index) {
+    _sessions.erase(index);
+    if (_devices.size() + _sessions.size() == 0) {
+        _active = false;
+        return true;
+    }
+    return false;
+}
+
+void Channel::clear() {
+    _active = false;
+    _devices.clear();
+    _sessions.clear();
+}
+
+void Channel::setVolume(float volume) {
+    _volume = volume;
+    for (auto &entry : _devices) 
+        _mgr->setSinkVolume(entry.first, _volume);
+    for (auto &entry : _sessions) 
+        _mgr->setSinkInputVolume(entry.first, _volume);
+}
+
+float Channel::getVolume() {
+    return _volume;
+}
+
+void Channel::setMute(bool mute) {
+    _mute = mute;
+    for (auto &entry : _devices) 
+        _mgr->setSinkMute(entry.first, _mute);
+    for (auto &entry : _sessions) 
+        _mgr->setSinkInputMute(entry.first, _mute);
+}
+
+bool Channel::getMute() {
+    return _mute;
+}
---
+++ linux/src/channel.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "globals.h"
+#include "session.h"
+#include "volume_manager.h"
+
+#include <map>
+#include <memory>
+
+class Channel {
+public:
+    Channel(std::shared_ptr<VolumeManager> mgr);
+    ~Channel();
+
+    bool addDevice(Session &device);
+    bool addSession(Session &session);
+    bool removeDevice(int index);
+    bool removeSession(int index);
+    void clear();
+
+    void setVolume(float volume);
+    float getVolume();
+    void setMute(bool mute);
+    bool getMute();
+
+private:
+    std::shared_ptr<VolumeManager> _mgr;
+    std::map<int, Session> _devices;
+    std::map<int, Session> _sessions;
+    bool _active;
+    float _volume;
+    bool _mute;
+};
